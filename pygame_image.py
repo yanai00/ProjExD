a1ca367ec5d7493cc73623c7962804ff0e21b147
yanai <c0a242648f@edu.teu.ac.jp>
2025-06-10 16:21:31 +0900

    課題10　移動

--- pygame_image.py
+++ pygame_image.py
@@ -14,16 +14,28 @@ def main():
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     flipped1 = pg.transform.flip(flipped1, True, False)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        key_list = pg.key.get_pressed()
+        if key_list[pg.K_UP]:
+            kk_rct.move_ip((0, -1))
+        if key_list[pg.K_DOWN]:
+            kk_rct.move_ip((0, 1))
+        if key_list[pg.K_LEFT]:
+            kk_rct.move_ip((-1, 0))
+        if key_list[pg.K_RIGHT]:
+            kk_rct.move_ip((1, 0))
+
         x=tmr%3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(flipped1, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(kk_img, [300,200])
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
