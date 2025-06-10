5d7c2057424cddb740b470c43ed1e3fa649eeeac
yanai <c0a242648f@edu.teu.ac.jp>
2025-06-10 15:47:04 +0900

    課題６　fps

--- pygame_image.py
+++ pygame_image.py
@@ -21,7 +21,7 @@ def main():
         screen.blit(kk_img, [300,200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
