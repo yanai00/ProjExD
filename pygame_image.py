4baa55353d87c2ba2504eab7bb8058414f2f62b6
yanai <c0a242648f@edu.teu.ac.jp>
2025-06-10 15:29:20 +0900

    課題４ こうかとん 画像読み込み

--- pygame_image.py
+++ pygame_image.py
@@ -18,6 +18,7 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, [300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
