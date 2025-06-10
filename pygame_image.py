341ac6d3b477786c34800a7171406977e02efbc5
yanai <c0a242648f@edu.teu.ac.jp>
2025-06-10 15:54:39 +0900

    課題7 連続

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg2_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
@@ -18,6 +19,7 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [-tmr, 0])
+        screen.blit(bg2_img, [-tmr+1600, 0])
         screen.blit(kk_img, [300,200])
         pg.display.update()
         tmr += 1        
